63e6938e07130e37137f0ba5dff2cd3520dadb7f
sandeep-7610 <sandeep.kella@zohocorp.com>
2020-06-06 13:37:48 +0530

    intial files

---
+++ app/src/main/java/com/sandeep_tosh/news/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sandeep_tosh.news
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
